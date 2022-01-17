64362ecf0da165d1a0b9024e6563aa95651ebfe6
ashleemarees <47039989+ashleemarees@users.noreply.github.com>
2022-01-17 07:40:32 -0700

    updates

--- onclick.js
+++ onclick.js
@@ -1,22 +1,20 @@
+// Get the modal
 var modal = document.getElementById("myModal");
-var i;
 
-var img = document.getElementsByClassName("myImg");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-
- for(i=0;i< img.length;i++)
-   {    
-    img[i].onclick = function(){
-
-    modal.style.display = "block";
-       modalImg.src = this.src;
-
- }
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-
-span.onclick = function() { 
-   modal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
